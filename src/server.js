ada6b6101e7792a4cc7d925d4319617558e71f93
kanjani8 <minjijb@gmail.com>
2021-08-15 15:23:07 +0900

    Update Server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,15 @@
 const express = require("express");
 
+const PORT = 4000;
 const app = express();
+
+const home = (req, res) => {
+    return res.send("hello");
+};
+
+app.get("/", home);
+
+const handleListening = () =>
+    console.log(`Server listening - port:${PORT}🧭🏝`);
+
+app.listen(PORT, handleListening);
