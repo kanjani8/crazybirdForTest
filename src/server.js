ca85e563ce737c1326fc3cc88373956ed04d20bc
HongChaeYoung99 <65443226+HongChaeYoung99@users.noreply.github.com>
2021-08-15 15:43:43 +0900

    morgan
    morgan install

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
+const logger = morgan("dev");
 
 const home = (req, res) => {
     return res.send("hello");
 };
 
+app.use(logger);
 app.get("/", home);
 
 const handleListening = () =>
