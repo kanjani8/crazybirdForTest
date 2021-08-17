9462f4e9514ac65558a0872b7ee43ec7c8b7129d
kanjani8 <minjijb@gmail.com>
2021-08-17 23:07:17 +0900

    login button changed with condition

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,1 +1,6 @@
-export const testbirdmain = (req, res) => res.render("home", {pageTitle: "main"});
+const fakeUser = {
+    username: "Nicolas",
+    loggedIn: true,
+  };
+
+export const testbirdmain = (req, res) => res.render("home", {pageTitle: "main", fakeUser});
