7d5d2930e55b7a99ffe7f735a31dc6c4962c7c2c
HongChaeYoung99 <65443226+HongChaeYoung99@users.noreply.github.com>
2021-08-21 15:44:10 +0900

    async

--- src/controllers/subjectController.js
+++ src/controllers/subjectController.js
@@ -5,7 +5,7 @@ export const see = (req, res) => res.send("subject seePage!");
 export const community = (req, res) => res.send("subject communityPage!");
 export const setting = (req, res) => res.send("subject settingPage!");
 export const register = (req, res) => res.send("subject registerPage!");
-export const test = (req, res) => {
+export const test = async (req, res) => {
   const tests = await Test.find({});
     return res.render("test", { pageTitle: "Test", tests });
   };
