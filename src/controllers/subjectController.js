3361d0c68ce025833663466c6df722290f274fce
HongChaeYoung99 <65443226+HongChaeYoung99@users.noreply.github.com>
2021-08-24 17:00:04 +0900

    update subject

--- src/controllers/subjectController.js
+++ src/controllers/subjectController.js
@@ -1,7 +1,20 @@
 import Test from "../models/test";
+import Subject from "../models/subject";
 
-export const search = (req, res) => res.send("subject searchPage!");
-export const see = (req, res) => res.send("subject seePage!");
+export const search = async (req, res) => {
+  const subjects = await Subject.find({});
+  return res.render("search", { pageTitle: "Subject Search", subjects });
+};
+export const see =  async (req, res) => {
+  const { id } = req.params;
+  const subject = await Subject.findById(id);
+  if (!subject) {
+    return res.render("404", { pageTitle: "Subject not found." });
+  }
+  return res.render("seeSubject", { pageTitle: subject.name, subject });
+};
+export const solve = (req, res) => res.send("subject solvePage!");
+export const list = (req, res) => res.send("subject listPage!");
 export const community = (req, res) => res.send("subject communityPage!");
 export const setting = (req, res) => res.send("subject settingPage!");
 export const register = (req, res) => res.send("subject registerPage!");
--- src/models/subject.js
+++ src/models/subject.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const subjectSchema = new mongoose.Schema({
   name: String, 
   describe: String,
+  professor: String
 });
 
 const Subject = mongoose.model("Subject", subjectSchema);
