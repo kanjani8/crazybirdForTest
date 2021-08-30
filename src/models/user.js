4287173fa136b5019f3fc90b513ce5988878696d
kanjani8 <minjijb@gmail.com>
2021-08-31 01:55:23 +0900

    enroll front end

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    userName: String,
-    password: Password,
-    realName: String,
-    
+    email: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true},
+    pasword: {type: String, required: true},
+    name: {type: String, required: true},
+    //university: {type: String, required: true}, 
 })
 
 const User = mongoose.model( User, userSchema);
