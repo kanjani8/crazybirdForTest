9ca7f968d06969e608b4f57166732b354401cc6b
Geronimo-pmj <geronimopmj@gmail.com>
2021-12-11 02:30:54 +0900

    miniCssExtractPlugin + dev tools setting

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,15 @@ const path = require("path");
 module.exports = {
     entry: "./src/client/js/main.js",
     mode: "development", // developnemt or production(개발완료/default)
+    watch: true,
+    plugins: [new MiniCssExtractPlugin({
+            filename: "css/styles.css",
+        }),
+    ],
     output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "assets", "js"),
+        filename: "js/main.js",
+        path: path.resolve(__dirname, "assets"),
+        clean: true,
     },
     module: {
         rules: [
@@ -21,7 +27,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
         ],
     },
