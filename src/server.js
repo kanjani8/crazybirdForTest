f3d926d5842c262d1003422711b42b15ccc9277a
Geronimo-pmj <geronimopmj@gmail.com>
2021-12-10 23:34:00 +0900

    scss and webpack

--- src/server.js
+++ src/server.js
@@ -36,7 +36,6 @@ app.use(
 // });
 app.use(localsMiddleware);
 //user can access to this file
-//app.use("/assets", express.static("assets"));
 app.use("/frontJS", express.static("frontJS"));
 app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
     entry: "./src/client/js/main.js",
-    mode: "development",
+    mode: "development", // developnemt or production(개발완료/default)
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "assets", "js"),
@@ -17,6 +18,10 @@ module.exports = {
                         presets: [["@babel/preset-env", { targets: "defaults"}]],
                     },
                 },
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
             },
         ],
     },
