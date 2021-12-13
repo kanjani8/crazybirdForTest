98e336f7f2b976e9d6e7cb397fbb3ba4d22360b8
HongChaeYoung99 <65443226+HongChaeYoung99@users.noreply.github.com>
2021-12-13 15:18:02 +0900

    videoPlayer

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,59 @@
+console.log("video");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const volumeRange = document.getElementById("volume");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+console.log(video);
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMuteClick = (e) => {
+    if(video.muted)
+    {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute":"Mute";
+    volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+    console.log(event.target.value);
+    const {
+      target: { value },
+    } = event;
+    if (video.muted) {
+      video.muted = false;
+      muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = value;
+  };
+const formatTime = (seconds) =>
+    new Date(seconds * 1000).toISOString().substr(11, 8);
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+  };
+  
+const handleTimeUpdate = () => {
+    currenTime.innerText = formatTime(Math.floor(video.currentTime));
+};
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
